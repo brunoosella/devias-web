cc990ed5ecca03d2e5812f40601930fd8142ee25
brunoosella08 <brunoosella08@gmail.com>
2023-06-29 16:40:45 -0300

    First commit

---
+++ devias-web/src/App.jsx
@@ -0,0 +1,13 @@
+// External Modules
+import React from 'react'
+
+// Internal nmodules
+import Home from './Screens/Home'
+
+export default function App() {
+
+  return (
+    <Home />
+  )
+
+}
---
+++ devias-web/src/Screens/Home.jsx
@@ -0,0 +1,15 @@
+// External modules
+import React from 'react'
+
+// Internal modules
+import './Home.scss'
+
+export default function Home(){
+
+  return(
+    <div id='home'>
+      <h1>Acá va nuestra Landing</h1>
+    </div>
+  )
+
+}
