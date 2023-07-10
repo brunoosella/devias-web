04be49c461d3c0972c42a3080a40e31eb0af66bc
brunoosella08 <brunoosella08@gmail.com>
2023-07-10 17:19:12 -0300

    fix lintern issues

--- devias-web/src/Components/Header/Header.jsx
+++ devias-web/src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 // External Modules
-import React, { useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Link, NavLink} from 'react-router-dom'
 
 // Internal modules
 import './Header.scss'
@@ -9,49 +9,53 @@ import './Header.scss'
 import logo from '../../Assets/logo.png'
 
 export default function Header() {
-    // Local state
-    const [isOpen, setIsOpen] = useState(false)
 
-    // Methods
-    const handleMenuToggle = () => {
-        setIsOpen(!isOpen);
-    }
+  // Local state
+  const [isOpen, setIsOpen] = useState(false)
 
-    return (
-        <header>
-            <nav className='container-header'>
-                <section className='container-header-logo'>
-                    <Link to='/'>
-                        <img src={logo} alt='dev-logo' />
-                    </Link>
-                </section>
-                <button className='menu-toggle' onClick={handleMenuToggle}>
-                    {/* Cambiar icono de menu */}
-                    <span>-</span>
-                </button>
-                <section className={`container-header-links ${isOpen ? 'show' : ''}`}>
-                    <ul>
-                        <li>
-                            <NavLink to='/'> Home </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/our-customers'> Our customers </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/about-us'> About us </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/our-way'> Our way </NavLink>
-                        </li>
-                    </ul>
-                </section>
-                <section className='container-header-button'>
-                    <button className='btn-contact'>
-                        <Link to='/'> Schedule a meeting </Link>
-                    </button>
-                </section>
-            </nav>
-        </header>
-    )
+  // Methods
+  const handleMenuToggle = () => {
+
+    setIsOpen(!isOpen)
+
+  }
+
+  return (
+    <header>
+      <nav className='container-header'>
+        <section className='container-header-logo'>
+          <Link to='/'>
+            <img src={logo} alt='dev-logo' />
+          </Link>
+        </section>
+        <button className='menu-toggle' onClick={handleMenuToggle}>
+          {/* Cambiar icono de menu */}
+          <span>-</span>
+        </button>
+        <section className={`container-header-links ${isOpen ? 'show' : ''}`}>
+          <ul>
+            <li>
+              <NavLink to='/'> Home </NavLink>
+            </li>
+            <li>
+              <NavLink to='/our-customers'> Our customers </NavLink>
+            </li>
+            <li>
+              <NavLink to='/about-us'> About us </NavLink>
+            </li>
+            <li>
+              <NavLink to='/our-way'> Our way </NavLink>
+            </li>
+          </ul>
+        </section>
+        <section className='container-header-button'>
+          <button className='btn-contact'>
+            <Link to='/'> Schedule a meeting </Link>
+          </button>
+        </section>
+      </nav>
+    </header>
+  )
+
 }
 
