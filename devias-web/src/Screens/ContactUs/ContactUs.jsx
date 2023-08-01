5bc528584557ffd42f8a5ebd7eff29ad7988d1a7
nico-villegas <nicovillegas650@gmail.com>
2023-08-01 19:16:54 -0300

    Add Calendly on screen contact us

--- devias-web/src/Screens/ContactUs/ContactUs.jsx
+++ devias-web/src/Screens/ContactUs/ContactUs.jsx
@@ -6,11 +6,13 @@ import './ContactUs.scss'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 
-export default function ContactUs () {
+export default function ContactUs() {
+  const calendlyLink = 'https://calendly.com/nicovillegas650/30min';
 
   return (
     <main id='contact-us'>
       <Header />
+      {/* Section title */}
       <section className='section-title'>
         <div className='container'>
           <div className='main-title'>
@@ -20,6 +22,27 @@ export default function ContactUs () {
             <h1>
               Let's talk.
             </h1>
+          </div>
+        </div>
+      </section>
+      {/* Section calendly */}
+      <section className='calendly'>
+        <div className='container'>
+          <p>
+            Choose a date and time for our meeting.
+          </p>
+          <p>
+            We will contact you to arrange the details.
+          </p>
+          {/* Insert calendly */}
+          <div className="calendly-container">
+            <iframe
+              title="Calendly"
+              src={calendlyLink}
+              width="100%"
+              height="600"
+              frameBorder="0"
+            />
           </div>
         </div>
       </section>
