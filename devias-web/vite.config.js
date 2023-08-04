cf806744efd8c6427c0e2eeacd73ee09ed99eb4e
brunoosella08 <brunoosella08@gmail.com>
2023-08-04 14:05:49 -0300

    Add aliases

--- devias-web/vite.config.js
+++ devias-web/vite.config.js
@@ -1,7 +1,16 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      Assets: path.resolve(__dirname, 'src/Assets'),
+      Components: path.resolve(__dirname, 'src/Components'),
+      Router: path.resolve(__dirname, 'src/Router'),
+      Screens: path.resolve(__dirname, 'src/Screens')
+    }
+  }
 })
