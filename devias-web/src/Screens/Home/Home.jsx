0b1fac834a10f9404f2022ead86fae67d782659b
brunoosella08 <brunoosella08@gmail.com>
2023-08-04 14:40:57 -0300

    Test deploy changes

--- devias-web/src/Screens/Home/Home.jsx
+++ devias-web/src/Screens/Home/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div className='container'>
           <div className='MainTitle'>
             <h1>
-              Humanizing
+              PROOF
               <br />
               digital products.
             </h1>
