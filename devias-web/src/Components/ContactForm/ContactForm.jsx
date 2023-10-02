7ed1175e74ff6780200dd34e00b1a380a27b57bb
nico-villegas <nicovillegas650@gmal.com>
2023-10-02 18:06:11 -0300

    Use of component

--- devias-web/src/Components/ContactForm/ContactForm.jsx
+++ devias-web/src/Components/ContactForm/ContactForm.jsx
@@ -10,6 +10,7 @@ export default function ContactForm (){
   //Constant
   const form = React.useRef()
 
+  // Request of EmailJS
   const sendEmail = (e) => {
 
     e.preventDefault()
@@ -32,36 +33,73 @@ export default function ContactForm (){
       <div className='container'>
         <div className='form-container'>
 
-          <div className='input-container'>
+          {/* Title formularie */}
+          <div className='contact-title'>
+
+            <h2>
+              Let's talk.
+            </h2>
+
+          </div>
+
+          {/* Container of data */}
+          <div className='data-container'>
 
             <div className='locker'>
-              <label className='form-label '>Name</label>
-              <input type='text' name='user_name' placeholder='Name' />
+              <label className='form-label '>Name <span>*</span></label>
+              <input
+                type='text'
+                name='user_name'
+                placeholder='Name'
+                required
+              />
             </div>
 
             <div className='locker'>
-              <label className='form-label' >Last name</label>
-              <input type='text' name='user_last_name' placeholder='Last name' />
+              <label className='form-label' >Last name <span>*</span></label>
+              <input
+                type='text'
+                name='user_last_name'
+                placeholder='Last name'
+                required
+              />
             </div>
 
             <div className='locker'>
 
-              <label className='form-label' >Company</label>
-              <input type='text' name='user_company' placeholder='Company' />
+              <label className='form-label' >Company <span>*</span></label>
+              <input
+                type='text'
+                name='user_company'
+                placeholder='Company'
+                required
+              />
             </div>
 
             <div className='locker'>
 
-              <label className='form-label' >E-mail</label>
-              <input type='email' name='user_email' placeholder='E-mail' />
+              <label className='form-label' >E-mail <span>*</span></label>
+              <input
+                type='email'
+                name='user_email'
+                placeholder='E-mail'
+                required
+              />
             </div>
           </div>
 
+          {/* Container of consult */}
           <div className='consult-container'>
 
-            <label className='form-label' >Consult</label>
-            <textarea name='consult' placeholder='Consult' />
-            <input type='submit' value='Send' />
+            <label className='form-label' >Consult <span>*</span></label>
+            <textarea
+              name='consult'
+              required
+            />
+            <input
+              type='submit'
+              value='Send'
+            />
           </div>
 
         </div>
