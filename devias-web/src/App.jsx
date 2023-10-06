868e5eacff06f4fcedd3b3fb0075e2f305590a5a
brunoosella08 <brunoosella08@gmail.com>
2023-10-06 16:43:48 -0300

    Add new router

--- devias-web/src/App.jsx
+++ devias-web/src/App.jsx
@@ -1,14 +1,32 @@
 // External Modules
 import React from 'react'
+import {RouterProvider} from 'react-router-dom'
 
 // Router
-import Router from './Router/Router'
+import router from './Router/routes'
 
 export default function App() {
 
+  // Local state
+  const [loading, setLoading] = React.useState(true)
+
+  // Mount effect
+  React.useEffect(() => {
+
+    // Update loading
+    setLoading(false)
+
+    // Config fontAwesome
+    const configFA = document.createElement('script')
+    configFA.innerHTML = 'FontAwesomeConfig = { autoReplaceSvg: "nest"};'
+    document.body.appendChild(configFA)
+
+  }, [])
+
   return (
 
-    <Router />
+    !loading &&
+      <RouterProvider router={router} />
 
   )
 
