7d4409da485a7213338aa9ecd27bf4a58b717776
brunoosella08 <brunoosella08@gmail.com>
2023-11-03 16:15:04 -0300

    Fix whatsapp link

--- devias-web/src/Components/WhatsappBubble/WhatsappBubble.jsx
+++ devias-web/src/Components/WhatsappBubble/WhatsappBubble.jsx
@@ -1,6 +1,6 @@
 //External modules
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 //Internal modules
 import './WhatsappBubble.scss'
@@ -9,11 +9,15 @@ import './WhatsappBubble.scss'
 import logoWhatsapp from 'Assets/Utilities/logo-whatsapp.png'
 
 export default function WhatsappBubble () {
+
   return (
+
     <div className='whatsapp-bubble'>
-      <Link>
+      <Link to='https://wa.me/543513464561' target='_blank'>
         <img src={logoWhatsapp} alt='logo-whatsapp' />
       </Link>
     </div>
+
   )
+
 }
